142a8402bcde3dc3c0b5d9fb5b15d3a67bc07587
mark <mlybarger@gmail.com>
2016-02-25 11:35:28 -0500

    fun with transactions.

--- src/main/java/com/example/switchyard/switchyard_example/ThreeBean.java
+++ src/main/java/com/example/switchyard/switchyard_example/ThreeBean.java
@@ -1,17 +1,43 @@
 package com.example.switchyard.switchyard_example;
 
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 import org.apache.log4j.Logger;
 import org.switchyard.component.bean.Service;
+
+import com.sample.model.Property;
 
 @Service(Three.class)
 public class ThreeBean implements Three {
 	
 	private static final Logger LOG = Logger.getLogger(ThreeBean.class);
-	private static final int SLEEP_SECONDS = 15;
+	private static final int SLEEP_SECONDS = 1;
+
+	@PersistenceContext(unitName="persistenceUnit")
+	EntityManager em;
 
 	@Override
 	public String process(String message) {
 		LOG.info("process() - start :" + message);
+		Property p = new Property();
+		p.setKey(new java.util.Date().getTime() + "");
+		p.setValue("message:" + message);
+		// do some persistence stuff here.
+		em.persist(p);
+		
+		Query q = em.createQuery("select p from Property p");
+		List<Property> propertyList = q.getResultList();
+		for (Property _p: propertyList) {
+			LOG.info("property:" + _p.getKey() + ":" + _p.getValue());
+		}
+		
+		LOG.info("before sleep");
+		
 		try {
 		    Thread.sleep(SLEEP_SECONDS * 1000);                 //1000 milliseconds is one second.
 		} catch(InterruptedException ex) {
